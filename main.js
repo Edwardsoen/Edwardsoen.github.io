4a937d0afef019c61de4d43512e073e936e4ab1c
Edward <edwardsoen@gmail.com>
2022-05-01 21:03:58 +0800

    add three js

--- main.js
+++ main.js
@@ -24,9 +24,6 @@ function main(){
   var ClickAudioPath = "./assets/sound kit/Tab 1.m4a"
   var HoverAudioPath = "./assets/sound kit/Button 2.m4a"
 
-
-
-
   var BorderAction = new OnBorderSelectAction()
   MainMenuNavigator.AddEventlistenerToList("mouseenter", Highlighter.HighlightOnSelect)
   MainMenuNavigator.AddEventlistenerToList("mouseenter", createPlayAudioFunction(HoverAudioPath))
@@ -35,11 +32,9 @@ function main(){
   MainMenuNavigator.AddEventlistenerToList("Deselected", BorderAction.onDeselectSetBorderIndvisible); 
   MainMenuNavigator.AddEventlistenerToList("click", MainMenuClickAction.OnClickNavigate); 
 
-
   var ProjectLists = document.getElementsByClassName("projecttitlebox"); 
   var ProjectListNavigator = new NavigationManager(ProjectLists, ProjectLists[0], false);
   var ProjectList = new ProjectListManager(); 
-
 
   ProjectListNavigator.AddEventlistenerToList("mouseenter", Highlighter.HighlightOnSelect)
   ProjectListNavigator.AddEventlistenerToList("mouseenter", createPlayAudioFunction(HoverAudioPath))
@@ -48,9 +43,7 @@ function main(){
   ProjectListNavigator.AddEventlistenerToList("click", createPlayAudioFunction(ClickAudioPath))
   ProjectListNavigator.AddEventlistenerToList("Selected", ProjectList.OnClickChangeDescriptionPanel)
   
-
   setCloseButtonAction()
-
 
   var ImageList = document.getElementsByClassName("imageitem"); 
   var ImageListNavigator = new NavigationManager(ImageList, ImageList[0])
@@ -66,6 +59,128 @@ function main(){
   parentPage.addEventListener("PageOpened", PageAction.onPageOpened)
   parentPage.addEventListener("PageClosed", PageAction.onPageClosed)
 }
+
+
+
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+function ThreeJSManager() { 
+  var scene; 
+  var camera; 
+  var renderer;
+  var box; 
+  var visible = 1.18; 
+  var tv; 
+  var initialize= function(){ 
+    scene = new THREE.Scene(); 
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
+    renderer = new THREE.WebGLRenderer({
+      canvas: document.getElementById("background")
+    }) 
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth,window.innerHeight)
+    camera.position.z = 1; 
+  }
+
+
+  function loadAsset() { 
+    const loader = new GLTFLoader();
+    loader.load( '/assets/TV.glb', function ( gltf ) {
+      scene.add( gltf.scene );
+      tv = gltf.scene; 
+      gltf.scene.z = -1; 
+      let defaultScale = 0.3
+      gltf.scene.scale.set(defaultScale,defaultScale,defaultScale); 
+      console.log(gltf.scene.position); 
+    }, undefined, function ( error ) {
+      console.error( error );
+    } );
+  } 
+
+  var AddHelpers = function() { 
+    var axesHelper = new THREE.AxesHelper( 5 );
+    // var lightHelper = new lightHelper(); 
+    // scene.add(lightHelper); 
+    scene.add( axesHelper);
+  }
+
+  function Addbox() { 
+      var Mest = new THREE.BoxGeometry(0.5,0.5,0.5); 
+      var mats = new THREE.MeshStandardMaterial(); 
+      box = new THREE.Mesh(Mest, mats); 
+      box.position.x = 0.2; 
+      scene.add(box); 
+  }
+
+  function AddLight() { 
+    const ambient= new THREE.AmbientLight(0x404040, 10); 
+    ambient.position.set( 0,0 ,1);
+    scene.add( ambient );
+  }
+
+
+  function animate() { 
+    if(tv != undefined) { 
+      // console.log(tv.position); 
+    // tv.position.y -= 0.01;
+    }
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+  } 
+  
+  function debugging(event){ 
+ 
+
+
+    if(event.keyCode == 37) { 
+      console.log("left")
+      tv.position.x -= 0.01; 
+    }
+    if(event.keyCode == 38) { 
+      console.log("up")
+      tv.position.y += 0.01; 
+    }
+    if(event.keyCode == 40) { 
+      console.log("down")
+      tv.position.y -= 0.01; 
+    }
+    if(event.keyCode == 39) { 
+      console.log("right")
+      tv.position.x += 0.01; 
+    }
+    if(event.keyCode == 49) { 
+      tv.scale.x -= 0.01; 
+      tv.scale.y -= 0.01;
+      tv.scale.z -= 0.01; 
+    }
+    if(event.keyCode == 50) { 
+      tv.scale.x += 0.01; 
+      tv.scale.y += 0.01;
+      tv.scale.z += 0.01; 
+    }
+    console.log(tv.scale)
+    console.log(tv.position)
+  }
+
+  document.addEventListener('keydown', debugging)
+
+
+
+  loadAsset(); 
+  initialize(); 
+  AddHelpers(); 
+  // Addbox(); 
+  AddLight(); 
+  animate(); 
+
+}
+
+
+
+var test = new ThreeJSManager(); 
+
+
 
 
 
