e9c34308e2cf0ee4b5df7754e1604a384029de4d
Edward <edwardsoen@gmail.com>
2022-08-28 14:20:46 +0800

    remove unused feature & add bg

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import './style.scss'
 import './animation.scss'
 import  {PageNavigator, NavigationManager} from './NavigationManager.js'
-import * as THREE from 'three'
+
+
+
 import {
   OnBorderSelectAction,
   createPlayAudioFunction,
@@ -24,15 +26,10 @@ function main(){
   var MainMenuNavigator = new NavigationManager(MainMenuList.children,MainMenuList.children[0]);
   var Highlighter = new OnSelectHighlighter(); 
 
-  var ClickAudioPath = "./assets/sound kit/Tab 1.m4a"
-  var HoverAudioPath = "./assets/sound kit/Button 2.m4a"
 
   var BorderAction = new OnBorderSelectAction()
   MainMenuNavigator.AddEventlistenerToList("mouseenter", Highlighter.HighlightOnSelect)
-  MainMenuNavigator.AddEventlistenerToList("mouseenter", createPlayAudioFunction(HoverAudioPath))
   MainMenuNavigator.AddEventlistenerToList("mouseleave", Highlighter.DeHighlightOnDeselct)
-  MainMenuNavigator.AddEventlistenerToList("Selected", BorderAction.onSelectSetBorderVisible); 
-  MainMenuNavigator.AddEventlistenerToList("Deselected", BorderAction.onDeselectSetBorderIndvisible); 
   MainMenuNavigator.AddEventlistenerToList("click", MainMenuClickAction.OnClickNavigate); 
 
   var ProjectLists = document.getElementsByClassName("projecttitlebox"); 
@@ -40,10 +37,8 @@ function main(){
   var ProjectList = new ProjectListManager(); 
 
   ProjectListNavigator.AddEventlistenerToList("mouseenter", Highlighter.HighlightOnSelect)
-  ProjectListNavigator.AddEventlistenerToList("mouseenter", createPlayAudioFunction(HoverAudioPath))
   ProjectListNavigator.AddEventlistenerToList("mouseleave", Highlighter.DeHighlightOnDeselct)
   ProjectListNavigator.AddEventlistenerToList("click", ProjectList.OnClickChangeDescriptionPanel)
-  ProjectListNavigator.AddEventlistenerToList("click", createPlayAudioFunction(ClickAudioPath))
   ProjectListNavigator.AddEventlistenerToList("Selected", ProjectList.OnClickChangeDescriptionPanel)
   
   setCloseButtonAction()
@@ -51,10 +46,8 @@ function main(){
   var ImageList = document.getElementsByClassName("imageitem"); 
   var ImageListNavigator = new NavigationManager(ImageList, ImageList[0])
   ImageListNavigator.AddEventlistenerToList("mouseenter", onSelectIncreaseSize)
-  ImageListNavigator.AddEventlistenerToList("mouseenter", createPlayAudioFunction(HoverAudioPath))
   ImageListNavigator.AddEventlistenerToList("mouseleave", onDeSelectDecreaseSize)
   ImageListNavigator.AddEventlistenerToList("click", onImageSelected)
-  ImageListNavigator.AddEventlistenerToList("click", createPlayAudioFunction(ClickAudioPath))
 
   var PageAction = new PageChangeActionManager()
   var parentPage = document.getElementsByClassName("parent")[0]
@@ -64,88 +57,7 @@ function main(){
 }
 
 
-function ThreeJSManager() { 
-  var scene; 
-  var camera; 
-  var renderer;
-  var sphere; 
-  var pointLight; 
-  var AmbientLight; 
-  var stars = []; 
 
-  var initialize= function(){ 
-    scene = new THREE.Scene(); 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
-    renderer = new THREE.WebGLRenderer({
-      canvas: document.getElementById("background")
-    }) 
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth,window.innerHeight)
-    camera.position.z = 1; 
-  }
-
-  function AddSphere() { 
-      var texture = new THREE.TextureLoader().load("./assets/Earth_Illumination_6K.jpg")
-      var geometry = new THREE.SphereGeometry(); 
-      var materials = new THREE.MeshStandardMaterial({map:texture}); 
-      sphere = new THREE.Mesh(geometry ,materials);
-      sphere.scale.set(0.5,0.5, 0.5) 
-      scene.add(sphere); 
-  }
-
-  function AddLight() { 
-    AmbientLight= new THREE.AmbientLight(0xffffff, 1); 
-    pointLight = new THREE.PointLight(0xffffff,1);
-    pointLight.position.set(5, 5, 5);
-    AmbientLight.position.set( 0,0 ,1);
-    scene.add( AmbientLight, pointLight );
-  }
-
-  function AddStars() { 
-    for(var i =0; i < 50; i ++) { 
-      const Stargeometry = new THREE.SphereGeometry(0.025, 0.01, 0.01);
-      const Starmaterial = new THREE.MeshStandardMaterial();
-      Starmaterial.roughness = 0; 
-      const star = new THREE.Mesh(Stargeometry, Starmaterial);
-      star.position.set(THREE.MathUtils.randFloatSpread(5), THREE.MathUtils.randFloatSpread(5), THREE.MathUtils.randFloatSpread(15));
-      stars.push(star);
-      scene.add(star);
-    }
-  }
-
-  function getRandomPositiveValue(colorValue){ 
-    if(colorValue  > 0)  return THREE.MathUtils.randFloat(-0.2, 0.2);  
-    return  THREE.MathUtils.randFloat(0, 0.2);
-  }
-
-  function animate() { 
-    for(let i =0; i < stars.length; i ++ ){ 
-      stars[i].position.x += THREE.MathUtils.randFloat(0, 0.0005) 
-      stars[i].position.y += THREE.MathUtils.randFloat(0, 0.0005) 
-      stars[i].position.z += THREE.MathUtils.randFloat(0, 0.0005)
-    }
-    requestAnimationFrame(animate)
-    sphere.rotation.x += 0.0005; 
-    sphere.rotation.y += 0.0005;
-    renderer.render(scene,camera)
-  } 
-  function scrollEvent() {
-    AmbientLight.color.r += getRandomPositiveValue(AmbientLight.color.r)
-    AmbientLight.color.g += getRandomPositiveValue(AmbientLight.color.g)
-    AmbientLight.color.b += getRandomPositiveValue(AmbientLight.color.b)
-  }
-  
-  initialize(); 
-  document.getElementsByClassName("parent")[0].addEventListener("scroll", scrollEvent)
-  AddStars(); 
-  AddSphere(); 
-  AddLight(); 
-  animate(); 
-}
-
-
-
-new ThreeJSManager(); 
 main();
 
 
