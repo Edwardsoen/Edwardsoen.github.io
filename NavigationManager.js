17ea36f3b2ab7520c603d9c6451f24e078736100
Edward <edwardsoen@gmail.com>
2022-11-06 04:43:33 +0800

    prettfy

--- NavigationManager.js
+++ NavigationManager.js
@@ -1,103 +1,111 @@
-function NavigationManager (list, FirstSelectedItem, SelectoOHover = true){
-    this.list = list; 
-    var selectedItem = FirstSelectedItem; 
+function NavigationManager(list, FirstSelectedItem, SelectoOHover = true) {
+  this.list = list;
+  var selectedItem = FirstSelectedItem;
 
-    var InitalizeEvents = function(){ 
-        const SelectedEvent = new CustomEvent("Selected", {detail:selectedItem}); 
-        selectedItem.dispatchEvent(SelectedEvent);
+  var InitalizeEvents = function () {
+    const SelectedEvent = new CustomEvent("Selected", { detail: selectedItem });
+    selectedItem.dispatchEvent(SelectedEvent);
+  };
+
+  var init = function () {
+    if (SelectoOHover) {
+      SetSelectedOnEachChildElementHover();
     }
+  };
 
-    var init = function(){ 
-        if(SelectoOHover) { 
-            SetSelectedOnEachChildElementHover();
-        }
+  this.setSelectedItemByIndex = function (index) {
+    this.selectedItem = list[index];
+  };
+
+  this.setSelectedItemByValue = function (value) {
+    if (list.includes(value)) {
+      this.selectedItem = value;
+      return;
     }
+    throw "Value Not in List";
+  };
 
-    this.setSelectedItemByIndex = function(index) { 
-        this.selectedItem = list[index]
+  this.AddEventlistenerToList = function (eventlistener, action) {
+    for (var i = 0; i < list.length; i++) {
+      list[i].addEventListener(eventlistener, action);
+      list[i].index = i;
     }
+    InitalizeEvents();
+  };
 
-    this.setSelectedItemByValue = function(value) { 
-        if(list.includes(value)) { 
-            this.selectedItem = value; 
-            return; 
-        }
-        throw "Value Not in List"; 
+  var setSelectedItemOnHover = function (event) {
+    const SelectedEvent = new CustomEvent("Selected", {
+      detail: event.currentTarget,
+    });
+    const DeSelectedEvent = new CustomEvent("Deselected", {
+      detail: selectedItem,
+    });
+    selectedItem.dispatchEvent(DeSelectedEvent);
+    selectedItem = list[event.currentTarget.index];
+    event.currentTarget.dispatchEvent(SelectedEvent);
+  };
+
+  var SetSelectedOnEachChildElementHover = function () {
+    for (var i = 0; i < list.length; i++) {
+      list[i].addEventListener("mouseenter", setSelectedItemOnHover);
     }
+  };
 
-    this.AddEventlistenerToList = function(eventlistener, action){
-        for(var i = 0; i < list.length; i ++){ 
-            list[i].addEventListener(eventlistener, action)
-            list[i].index = i
-        }
-        InitalizeEvents()
-    }
-
-    var setSelectedItemOnHover = function(event){ 
-        const SelectedEvent = new CustomEvent("Selected", {detail:event.currentTarget}); 
-        const DeSelectedEvent = new CustomEvent("Deselected",{detail:selectedItem})
-        selectedItem.dispatchEvent(DeSelectedEvent);
-        selectedItem = list[event.currentTarget.index]; 
-        event.currentTarget.dispatchEvent(SelectedEvent); 
-    }
-
-    var SetSelectedOnEachChildElementHover = function(){ 
-        for(var i =0; i < list.length; i ++){ 
-            list[i].addEventListener("mouseenter", setSelectedItemOnHover)
-        }
-    }
-    
-    init(); 
-} 
-
+  init();
+}
 
 function setColor(color) {
-    document.getElementById('debugger').style.backgroundColor = color
+  document.getElementById("debugger").style.backgroundColor = color;
 }
 
-function PageNavigator(parentElement, totalPage){ 
-    this.parent = parentElement
-    this.totalPage = totalPage
-  
-    var parentHeight = this.parent.scrollHeight; 
-    var heightPerPage = parentHeight/this.totalPage; 
-    var currentPage = 1; 
-    var currentY = 0
-  
-    var reCalculateHeight = function() { 
-        heightPerPage = parentElement.scrollHeight/totalPage; 
-    }
+function PageNavigator(parentElement, totalPage) {
+  this.parent = parentElement;
+  this.totalPage = totalPage;
 
-    
-    var FunctionOnScroll = function(event){
-        reCalculateHeight() 
-      if(currentY > event.currentTarget.scrollTop) { 
-        if(((event.currentTarget.scrollTop / heightPerPage)% 1) < 0.1) { 
-            let pageClosedEvent = new CustomEvent("PageClosed", {detail:currentPage})   
-            event.currentTarget.dispatchEvent(pageClosedEvent);
-            currentPage = Math.round(event.currentTarget.scrollTop / heightPerPage) + 1
-            let pageOpenEvent = new CustomEvent("PageOpened", {detail:currentPage})   
-            event.currentTarget.dispatchEvent(pageOpenEvent);        
-        }
-      }else { 
-            if(currentPage != (Math.round(event.currentTarget.scrollTop / heightPerPage) + 1)) {
-            let pageClosedEvent = new CustomEvent("PageClosed", {detail:currentPage})   
-            event.currentTarget.dispatchEvent(pageClosedEvent); 
-            currentPage = Math.round(event.currentTarget.scrollTop / heightPerPage) + 1
-            let pageOpenEvent = new CustomEvent("PageOpened", {detail:currentPage})   
-            event.currentTarget.dispatchEvent(pageOpenEvent); 
-        }
+  var parentHeight = this.parent.scrollHeight;
+  var heightPerPage = parentHeight / this.totalPage;
+  var currentPage = 1;
+  var currentY = 0;
+
+  var reCalculateHeight = function () {
+    heightPerPage = parentElement.scrollHeight / totalPage;
+  };
+
+  var FunctionOnScroll = function (event) {
+    reCalculateHeight();
+    if (currentY > event.currentTarget.scrollTop) {
+      if ((event.currentTarget.scrollTop / heightPerPage) % 1 < 0.1) {
+        let pageClosedEvent = new CustomEvent("PageClosed", {
+          detail: currentPage,
+        });
+        event.currentTarget.dispatchEvent(pageClosedEvent);
+        currentPage =
+          Math.round(event.currentTarget.scrollTop / heightPerPage) + 1;
+        let pageOpenEvent = new CustomEvent("PageOpened", {
+          detail: currentPage,
+        });
+        event.currentTarget.dispatchEvent(pageOpenEvent);
       }
-      currentY = event.currentTarget.scrollTop; 
+    } else {
+      if (
+        currentPage !=
+        Math.round(event.currentTarget.scrollTop / heightPerPage) + 1
+      ) {
+        let pageClosedEvent = new CustomEvent("PageClosed", {
+          detail: currentPage,
+        });
+        event.currentTarget.dispatchEvent(pageClosedEvent);
+        currentPage =
+          Math.round(event.currentTarget.scrollTop / heightPerPage) + 1;
+        let pageOpenEvent = new CustomEvent("PageOpened", {
+          detail: currentPage,
+        });
+        event.currentTarget.dispatchEvent(pageOpenEvent);
+      }
     }
-    this.parent.addEventListener("scroll", FunctionOnScroll)
+    currentY = event.currentTarget.scrollTop;
+  };
+  this.parent.addEventListener("scroll", FunctionOnScroll);
 }
-    
 
-
-
-
-export {NavigationManager, PageNavigator}; 
-
-
-
+export { NavigationManager, PageNavigator };
--- main.js
+++ main.js
@@ -1,84 +1,102 @@
-import './style.scss'
-import './animation.scss'
-import  {PageNavigator, NavigationManager} from './NavigationManager.js'
+import "./style.scss";
+import "./animation.scss";
+import { PageNavigator, NavigationManager } from "./NavigationManager.js";
 
 import {
   OnBorderSelectAction,
-  PageChangeActionManager, 
+  PageChangeActionManager,
   MainMenuClickActionManager,
   OnSelectHighlighter,
-  setCloseButtonAction, 
+  setCloseButtonAction,
   onImageSelected,
   ProjectListManager,
-  onDeSelectDecreaseSize 
-} from './ActionManager.js'
-
+  onDeSelectDecreaseSize,
+} from "./ActionManager.js";
 
 function generateIndividualCharacters(GrandParent, string) {
-  parent = document.createElement('h1')
-  for(let i = 0; i <= string.length -1 ; i ++) {
-      let char = document.createElement('span')
-      let letter = document.createElement('h1')
-      letter.textContent = string[i]
-      char.innerHTML = string[i]
-      char.classList.add('character')
-      parent.appendChild(char)
-    }
-    GrandParent.appendChild(parent)
-} 
-
-
-function main(){ 
-  var ProjectTitleContainer = document.getElementById("page2titlecontainer"); 
-  var HobbyTitleContainer = document.getElementById("page3titlecontainer"); 
-  var  ContactTitleContainer = document.getElementById("page4titlecontainer"); 
-
-
-  generateIndividualCharacters(ProjectTitleContainer, "Projects");
-  generateIndividualCharacters(HobbyTitleContainer, "Hobby"); 
-  generateIndividualCharacters(ContactTitleContainer, "Contact");
-
-  var MainMenuClickAction = new MainMenuClickActionManager(); 
-  var MainMenuList = document.getElementById("MainMenuItem"); 
-  var MainMenuNavigator = new NavigationManager(MainMenuList.children,MainMenuList.children[0]);
-  var Highlighter = new OnSelectHighlighter(); 
-
-
-  var BorderAction = new OnBorderSelectAction()
-  MainMenuNavigator.AddEventlistenerToList("mouseenter", Highlighter.HighlightOnSelect)
-  MainMenuNavigator.AddEventlistenerToList("mouseleave", Highlighter.DeHighlightOnDeselct)
-  MainMenuNavigator.AddEventlistenerToList("click", MainMenuClickAction.OnClickNavigate); 
-
-  
-  var ProjectList = new ProjectListManager(); 
-  var ProjectLists = document.getElementsByClassName("projecttitlebox"); 
-  var ProjectListNavigator = new NavigationManager(ProjectLists, ProjectLists[0], false);
-  
-
-  ProjectListNavigator.AddEventlistenerToList("mouseenter", Highlighter.HighlightOnSelect)
-  ProjectListNavigator.AddEventlistenerToList("mouseleave", Highlighter.DeHighlightOnDeselct)
-  ProjectListNavigator.AddEventlistenerToList("click", ProjectList.OnClickChangeDescriptionPanel)
-  ProjectListNavigator.AddEventlistenerToList("Selected", ProjectList.OnClickChangeDescriptionPanel)
-  
-  setCloseButtonAction()
-
-  var ImageList = document.getElementsByClassName("imageitem"); 
-  var ImageListNavigator = new NavigationManager(ImageList, ImageList[0])
-  ImageListNavigator.AddEventlistenerToList("mouseleave", onDeSelectDecreaseSize)
-  ImageListNavigator.AddEventlistenerToList("click", onImageSelected)
-
-  var PageAction = new PageChangeActionManager()
-  var parentPage = document.getElementsByClassName("parent")[0]
-  var pagenavigator = new PageNavigator(parentPage, 4); 
-  parentPage.addEventListener("PageOpened", PageAction.onPageOpened)
-  parentPage.addEventListener("PageClosed", PageAction.onPageClosed)
+  parent = document.createElement("h1");
+  for (let i = 0; i <= string.length - 1; i++) {
+    let char = document.createElement("span");
+    let letter = document.createElement("h1");
+    letter.textContent = string[i];
+    char.innerHTML = string[i];
+    char.classList.add("character");
+    parent.appendChild(char);
+  }
+  GrandParent.appendChild(parent);
 }
 
+function main() {
+  var ProjectTitleContainer = document.getElementById("page2titlecontainer");
+  var HobbyTitleContainer = document.getElementById("page3titlecontainer");
+  var ContactTitleContainer = document.getElementById("page4titlecontainer");
+
+  generateIndividualCharacters(ProjectTitleContainer, "Projects");
+  generateIndividualCharacters(HobbyTitleContainer, "Hobby");
+  generateIndividualCharacters(ContactTitleContainer, "Contact");
+
+  var MainMenuClickAction = new MainMenuClickActionManager();
+  var MainMenuList = document.getElementById("MainMenuItem");
+  var MainMenuNavigator = new NavigationManager(
+    MainMenuList.children,
+    MainMenuList.children[0]
+  );
+  var Highlighter = new OnSelectHighlighter();
+
+  var BorderAction = new OnBorderSelectAction();
+  MainMenuNavigator.AddEventlistenerToList(
+    "mouseenter",
+    Highlighter.HighlightOnSelect
+  );
+  MainMenuNavigator.AddEventlistenerToList(
+    "mouseleave",
+    Highlighter.DeHighlightOnDeselct
+  );
+  MainMenuNavigator.AddEventlistenerToList(
+    "click",
+    MainMenuClickAction.OnClickNavigate
+  );
+
+  var ProjectList = new ProjectListManager();
+  var ProjectLists = document.getElementsByClassName("projecttitlebox");
+  var ProjectListNavigator = new NavigationManager(
+    ProjectLists,
+    ProjectLists[0],
+    false
+  );
+
+  ProjectListNavigator.AddEventlistenerToList(
+    "mouseenter",
+    Highlighter.HighlightOnSelect
+  );
+  ProjectListNavigator.AddEventlistenerToList(
+    "mouseleave",
+    Highlighter.DeHighlightOnDeselct
+  );
+  ProjectListNavigator.AddEventlistenerToList(
+    "click",
+    ProjectList.OnClickChangeDescriptionPanel
+  );
+  ProjectListNavigator.AddEventlistenerToList(
+    "Selected",
+    ProjectList.OnClickChangeDescriptionPanel
+  );
+
+  setCloseButtonAction();
+
+  var ImageList = document.getElementsByClassName("imageitem");
+  var ImageListNavigator = new NavigationManager(ImageList, ImageList[0]);
+  ImageListNavigator.AddEventlistenerToList(
+    "mouseleave",
+    onDeSelectDecreaseSize
+  );
+  ImageListNavigator.AddEventlistenerToList("click", onImageSelected);
+
+  var PageAction = new PageChangeActionManager();
+  var parentPage = document.getElementsByClassName("parent")[0];
+  var pagenavigator = new PageNavigator(parentPage, 4);
+  parentPage.addEventListener("PageOpened", PageAction.onPageOpened);
+  parentPage.addEventListener("PageClosed", PageAction.onPageClosed);
+}
 
 main();
-
-
-
-
-
-
